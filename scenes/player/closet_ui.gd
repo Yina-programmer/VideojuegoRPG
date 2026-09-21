fea96903381c2ad363c630089c2a50a53ad68d10
Yina-programmer <yinnaardilaromero@gmail.com>
2026-09-20 20:48:49 -0500

    Actualiza armario, outfits y sistema de cabello

--- scenes/player/closet_ui.gd
+++ scenes/player/closet_ui.gd
@@ -1,57 +1,183 @@
 extends Control
 
+const PLAYER_SCENE = "res://scenes/player/player.tscn"
+
 @onready var preview_player = $ClosetPanel/CharacterPanel/player
+@onready var selection_label: Label = $ClosetPanel/CharacterPanel/SelectionLabel
+@onready var section_title: Label = $ClosetPanel/InventoryPanel/InventoryHeader/SectionTitle
+@onready var counter_label: Label = $ClosetPanel/InventoryPanel/InventoryHeader/CounterLabel
+@onready var status_label: Label = $ClosetPanel/InventoryPanel/StatusLabel
+@onready var outfits_grid: GridContainer = $ClosetPanel/InventoryPanel/OutfitsGrid
+@onready var hair_grid: GridContainer = $ClosetPanel/InventoryPanel/HairGrid
+@onready var palette_label: Label = $ClosetPanel/InventoryPanel/PaletteLabel
+@onready var color_row: HBoxContainer = $ClosetPanel/InventoryPanel/ColorRow
+@onready var outfits_tab: Button = $ClosetPanel/CategoryPanel/OutfitsTab
+@onready var hair_tab: Button = $ClosetPanel/CategoryPanel/HairTab
+@onready var save_button: Button = $ClosetPanel/SaveButton
 
-@onready var original_button = $ClosetPanel/OutfitsPanel/OutfitsGrid/OriginalButton
-@onready var pink_button = $ClosetPanel/OutfitsPanel/OutfitsGrid/PinkButton
-@onready var blue_button = $ClosetPanel/OutfitsPanel/OutfitsGrid/BlueButton
-@onready var black_button = $ClosetPanel/OutfitsPanel/OutfitsGrid/BlackButton
-@onready var white_bow_button = $ClosetPanel/OutfitsPanel/OutfitsGrid/WhiteBowButton
-@onready var black_bow_button = $ClosetPanel/OutfitsPanel/OutfitsGrid/BlackBowButton
+@onready var outfit_buttons = {
+	"original": $ClosetPanel/InventoryPanel/OutfitsGrid/OriginalButton,
+	"rosa": $ClosetPanel/InventoryPanel/OutfitsGrid/PinkButton,
+	"azul": $ClosetPanel/InventoryPanel/OutfitsGrid/BlueButton,
+	"negro": $ClosetPanel/InventoryPanel/OutfitsGrid/BlackButton,
+	"blanco_mono": $ClosetPanel/InventoryPanel/OutfitsGrid/WhiteBowButton,
+	"negro_mono": $ClosetPanel/InventoryPanel/OutfitsGrid/BlackBowButton
+}
+
+@onready var hair_buttons = {
+	"castano": $ClosetPanel/InventoryPanel/HairGrid/BrownHairButton,
+	"trenzas_rojas": $ClosetPanel/InventoryPanel/HairGrid/RedBraidsButton,
+	"flor_verde": $ClosetPanel/InventoryPanel/HairGrid/GreenFlowerButton
+}
+
+var outfit_names = {
+	"original": "Clásico",
+	"rosa": "Rosa",
+	"azul": "Azul",
+	"negro": "Negro",
+	"blanco_mono": "Lazo blanco",
+	"negro_mono": "Lazo negro"
+}
+
+var hair_names = {
+	"castano": "Castaño",
+	"trenzas_rojas": "Trenzas rojas",
+	"flor_verde": "Flor verde"
+}
 
 
-func _ready():
-	# El personaje de la izquierda es solo una vista previa.
+func _ready() -> void:
 	preview_player.set_physics_process(false)
 	preview_player.set_process_input(false)
+	preview_player.set_process_unhandled_input(false)
+	_configure_button_interaction()
 
-	original_button.pressed.connect(_on_original_pressed)
-	pink_button.pressed.connect(_on_pink_pressed)
-	blue_button.pressed.connect(_on_blue_pressed)
-	black_button.pressed.connect(_on_black_pressed)
-	white_bow_button.pressed.connect(_on_white_bow_pressed)
-	black_bow_button.pressed.connect(_on_black_bow_pressed)
+	for outfit_name in outfit_buttons:
+		outfit_buttons[outfit_name].pressed.connect(_select_outfit.bind(outfit_name))
+	for hair_name in hair_buttons:
+		hair_buttons[hair_name].pressed.connect(_select_hair.bind(hair_name))
 
-	cambiar_preview("original")
+	outfits_tab.pressed.connect(_show_outfits)
+	hair_tab.pressed.connect(_show_hair)
+	$ClosetPanel/CharacterPanel/PreviousCharacter.pressed.connect(_character_arrow_pressed)
+	$ClosetPanel/CharacterPanel/NextCharacter.pressed.connect(_character_arrow_pressed)
+	$ClosetPanel/BackButton.pressed.connect(_go_back)
+	save_button.pressed.connect(_save_customization)
+
+	var swatches = $ClosetPanel/InventoryPanel/ColorRow.get_children()
+	var swatch_outfits = ["original", "rosa", "azul", "negro", "blanco_mono", "negro_mono"]
+	for index in swatches.size():
+		swatches[index].pressed.connect(_select_outfit.bind(swatch_outfits[index]))
+
+	preview_player.lastDirection = "down"
+	_sync_selected_buttons()
+	_show_outfits()
+	_refresh_preview()
 
 
-func cambiar_preview(outfit_name: String):
+func _configure_button_interaction() -> void:
+	var buttons: Array[Button] = [
+		outfits_tab,
+		hair_tab,
+		$ClosetPanel/CharacterPanel/PreviousCharacter,
+		$ClosetPanel/CharacterPanel/NextCharacter,
+		$ClosetPanel/BackButton,
+		save_button
+	]
+	for button in outfit_buttons.values():
+		buttons.append(button)
+	for button in hair_buttons.values():
+		buttons.append(button)
+	for button in color_row.get_children():
+		buttons.append(button)
+
+	for button in buttons:
+		button.disabled = false
+		button.mouse_filter = Control.MOUSE_FILTER_STOP
+		button.action_mode = BaseButton.ACTION_MODE_BUTTON_PRESS
+		button.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
+
+
+func _select_outfit(outfit_name: String) -> void:
 	preview_player.cambiar_outfit(outfit_name)
+	outfit_buttons[outfit_name].button_pressed = true
+	status_label.text = "Conjunto seleccionado: " + outfit_names[outfit_name]
+	_refresh_preview()
+
+
+func _select_hair(hair_name: String) -> void:
+	preview_player.cambiar_cabello(hair_name)
+	hair_buttons[hair_name].button_pressed = true
+	status_label.text = "Cabello seleccionado: " + hair_names[hair_name]
+	_refresh_preview()
+
+
+func _refresh_preview() -> void:
 	preview_player.lastDirection = "down"
 	preview_player.body.animation = "walk_down"
 	preview_player.body.frame = 0
 	preview_player.body.stop()
+	preview_player.actualizar_cabello_visual()
+	selection_label.text = "%s  •  %s" % [
+		outfit_names.get(preview_player.outfit_actual, preview_player.outfit_actual),
+		hair_names.get(preview_player.cabello_actual, preview_player.cabello_actual)
+	]
+	_update_counter()
 
 
-func _on_original_pressed():
-	cambiar_preview("original")
+func _show_outfits() -> void:
+	outfits_grid.visible = true
+	hair_grid.visible = false
+	palette_label.visible = true
+	color_row.visible = true
+	outfits_tab.button_pressed = true
+	section_title.text = "OUTFITS"
+	status_label.text = "Elige un outfit o selecciona su color"
+	_update_counter()
 
 
-func _on_pink_pressed():
-	cambiar_preview("rosa")
+func _show_hair() -> void:
+	outfits_grid.visible = false
+	hair_grid.visible = true
+	palette_label.visible = false
+	color_row.visible = false
+	hair_tab.button_pressed = true
+	section_title.text = "CABELLO"
+	status_label.text = "Elige uno de los tres estilos de cabello"
+	_update_counter()
 
 
-func _on_blue_pressed():
-	cambiar_preview("azul")
+func _update_counter() -> void:
+	if hair_grid.visible:
+		var hair_keys = hair_buttons.keys()
+		counter_label.text = "%d / %d" % [hair_keys.find(preview_player.cabello_actual) + 1, hair_keys.size()]
+	else:
+		var outfit_keys = outfit_buttons.keys()
+		counter_label.text = "%d / %d" % [outfit_keys.find(preview_player.outfit_actual) + 1, outfit_keys.size()]
 
 
-func _on_black_pressed():
-	cambiar_preview("negro")
+func _sync_selected_buttons() -> void:
+	if outfit_buttons.has(preview_player.outfit_actual):
+		outfit_buttons[preview_player.outfit_actual].button_pressed = true
+	if hair_buttons.has(preview_player.cabello_actual):
+		hair_buttons[preview_player.cabello_actual].button_pressed = true
 
 
-func _on_white_bow_pressed():
-	cambiar_preview("blanco_mono")
+func _character_arrow_pressed() -> void:
+	status_label.text = "Por ahora solo está disponible el personaje mujer"
 
 
-func _on_black_bow_pressed():
-	cambiar_preview("negro_mono")
+func _save_customization() -> void:
+	var result = preview_player.guardar_personalizacion()
+	if result == OK:
+		status_label.text = "¡Personalización guardada!"
+		save_button.text = "GUARDADO"
+		await get_tree().create_timer(1.2).timeout
+		if is_instance_valid(save_button):
+			save_button.text = "GUARDAR"
+	else:
+		status_label.text = "No se pudo guardar. Inténtalo nuevamente."
+
+
+func _go_back() -> void:
+	get_tree().change_scene_to_file(PLAYER_SCENE)
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,12 +1,16 @@
 extends CharacterBody2D
 
-@onready var body: AnimatedSprite2D = $visual/body
-@onready var hair_down: AnimatedSprite2D = $visual/hair_down
-@onready var hair_left: AnimatedSprite2D = $visual/hair_left
-@onready var hair_right: AnimatedSprite2D = $visual/hair_right
-@onready var hair_up: AnimatedSprite2D = $visual/hair_up
-@onready var visual: Node2D = $visual
+signal personalizacion_cambiada(outfit_name: String, hair_name: String)
 
+const CUSTOMIZATION_PATH = "user://player_customization.cfg"
+
+@onready var body: AnimatedSprite2D = $visual/body
+@onready var visual: Node2D = $visual
+@onready var hair_styles = {
+	"castano": $visual/hair_castano,
+	"flor_verde": $visual/hair_flor_verde,
+	"trenzas_rojas": $visual/hair_trenzas_rojas
+}
 
 var speed = 400.0
 var lastDirection = "down"
@@ -21,101 +25,127 @@ var outfits = {
 }
 
 var outfit_actual: String = "original"
+var cabello_actual: String = "castano"
 
-func _process(_delta):
-	var cuerpo = get_node_or_null("visual/body")
-	var down = get_node_or_null("visual/hair_down")
-	var left = get_node_or_null("visual/hair_left")
-	var right = get_node_or_null("visual/hair_right")
-	var up = get_node_or_null("visual/hair_up")
 
-	if cuerpo == null or down == null or left == null or right == null or up == null:
-		return
+func _ready() -> void:
+	cargar_personalizacion()
 
-	down.visible = false
-	left.visible = false
-	right.visible = false
-	up.visible = false
 
-	match cuerpo.animation:
-		"walk_down":
-			down.animation = "walk_down"
-			down.frame = cuerpo.frame
-			down.visible = true
+func _process(_delta) -> void:
+	actualizar_cabello_visual()
 
-		"walk_left":
-			left.animation = "walk_left"
-			left.frame = cuerpo.frame
-			left.visible = true
 
-		"walk_right":
-			right.animation = "walk_right"
-			right.frame = cuerpo.frame
-			right.visible = true
-
-		"walk_up":
-			up.animation = "walk_up"
-			up.frame = cuerpo.frame
-			up.visible = true
-
-func _physics_process(_delta):
+func _physics_process(_delta) -> void:
 	getInput()
 	move_and_slide()
 
-func getInput():
+
+func getInput() -> void:
 	var inputDirection = Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown")
 	if inputDirection == Vector2.ZERO:
 		velocity = Vector2.ZERO
 		updateAnimation("walk")
 		return
 
-
 	if abs(inputDirection.x) > abs(inputDirection.y):
-		#Movimineto horizontal
-		if inputDirection.x > 0:
-			lastDirection = "right"
-		else:
-			lastDirection = "left"
+		lastDirection = "right" if inputDirection.x > 0 else "left"
 	else:
-		if inputDirection.y > 0:
-			lastDirection = "down"
-		else:
-			lastDirection = "up"
+		lastDirection = "down" if inputDirection.y > 0 else "up"
+
 	velocity = speed * inputDirection
-	
 	updateAnimation("walk")
 
-func updateAnimation (state):
-	body.play(state + "_" + lastDirection)
-func cambiar_outfit(nombre: String):
+
+func updateAnimation(state: String) -> void:
+	var animation_name = state + "_" + lastDirection
+	body.play(animation_name)
+
+
+func actualizar_cabello_visual() -> void:
+	for hair_style in hair_styles.values():
+		hair_style.visible = false
+
+	var selected_style: Node2D = hair_styles.get(cabello_actual)
+	if selected_style == null:
+		return
+
+	selected_style.visible = true
+	for hair_sprite in selected_style.get_children():
+		if hair_sprite is AnimatedSprite2D:
+			hair_sprite.visible = false
+
+	var direction := "down"
+	match body.animation:
+		"walk_left":
+			direction = "left"
+		"walk_right":
+			direction = "right"
+		"walk_up":
+			direction = "up"
+
+	var active_hair: AnimatedSprite2D = selected_style.get_node("hair_" + direction)
+
+	active_hair.animation = body.animation
+	active_hair.frame = body.frame
+	active_hair.frame_progress = body.frame_progress
+	active_hair.visible = true
+
+
+func cambiar_outfit(nombre: String) -> void:
 	if not outfits.has(nombre):
-		print("No existe el outfit: ", nombre)
+		push_warning("No existe el outfit: " + nombre)
 		return
 
 	body.sprite_frames = outfits[nombre]
 	outfit_actual = nombre
-
-	print("Outfit cambiado a: ", outfit_actual)
-	print("Recurso: ", body.sprite_frames.resource_path)
-
 	body.play("walk_" + lastDirection)
-func _unhandled_key_input(event):
+	personalizacion_cambiada.emit(outfit_actual, cabello_actual)
+
+
+func cambiar_cabello(nombre: String) -> void:
+	if not hair_styles.has(nombre):
+		push_warning("No existe el cabello: " + nombre)
+		return
+
+	cabello_actual = nombre
+	actualizar_cabello_visual()
+	personalizacion_cambiada.emit(outfit_actual, cabello_actual)
+
+
+func guardar_personalizacion() -> Error:
+	var config = ConfigFile.new()
+	config.set_value("character", "type", "mujer")
+	config.set_value("character", "outfit", outfit_actual)
+	config.set_value("character", "hair", cabello_actual)
+	return config.save(CUSTOMIZATION_PATH)
+
+
+func cargar_personalizacion() -> void:
+	var config = ConfigFile.new()
+	if config.load(CUSTOMIZATION_PATH) != OK:
+		return
+
+	var saved_outfit = str(config.get_value("character", "outfit", outfit_actual))
+	var saved_hair = str(config.get_value("character", "hair", cabello_actual))
+	if outfits.has(saved_outfit):
+		cambiar_outfit(saved_outfit)
+	if hair_styles.has(saved_hair):
+		cambiar_cabello(saved_hair)
+
+
+func _unhandled_key_input(event) -> void:
 	if event is InputEventKey and event.pressed and not event.echo:
 		match event.physical_keycode:
 			KEY_1, KEY_KP_1:
 				cambiar_outfit("original")
-
 			KEY_2, KEY_KP_2:
 				cambiar_outfit("rosa")
-
 			KEY_3, KEY_KP_3:
 				cambiar_outfit("azul")
-
 			KEY_4, KEY_KP_4:
 				cambiar_outfit("negro")
-
 			KEY_5, KEY_KP_5:
 				cambiar_outfit("blanco_mono")
-
 			KEY_6, KEY_KP_6:
 				cambiar_outfit("negro_mono")
